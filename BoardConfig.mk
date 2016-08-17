0a9dcbd653a5d2f17220505942cba78231a715dc
cb <billchen1977@gmail.com>
2016-08-17 10:30:23 +0800

    kenzo: remove earlyprintk causes longer boot
    Change-Id: I5cbfeaf0d155ef9d7c010fbf7f2c2ff237d99de2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,7 @@ TARGET_USES_64_BIT_BINDER := true
 ENABLE_CPUSETS := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_BASE        := 0x80000000
