7122093af4f07aaaccf7ea61f8653cf81e6050f5
Mridul Singh <mridulsingh@codeaurora.org>
2017-02-03 21:30:13 +0800

    QCamera2:HAL1: Enabling early restart for NZSL snapshot. Issue: When early restart is enabled, preview is started but frames are not enqueued to display. Fix: Add a check that when early restart is enabled to queue the frames to display.

--- camera/QCamera2/HAL/QCamera2HWI.cpp
+++ camera/QCamera2/HAL/QCamera2HWI.cpp
@@ -8861,9 +8861,10 @@ bool QCamera2HardwareInterface::isCaptureShutterEnabled()
  *==========================================================================*/
 bool QCamera2HardwareInterface::needProcessPreviewFrame()
 {
-    return m_stateMachine.isPreviewRunning()
+    return ((m_stateMachine.isPreviewRunning()
             && mParameters.isDisplayFrameNeeded()
-            && !mParameters.isInstantAECEnabled();
+            && (!mParameters.isInstantAECEnabled()))
+            || isPreviewRestartEnabled());
 };
 
 /*===========================================================================
