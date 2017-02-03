56c025d9631a4434af73b741eb356d511a0f7755
xyyx <xyyx@mail.ru>
2017-02-03 15:19:41 +0800

    Remove SafetyNet hack. Fixed in kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 26838785024 # 26838801408 - 16384
 
 # Kernel
 TARGET_KERNEL_CONFIG := cyanogenmod_kenzo_defconfig
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive androidboot.verifiedbootstate=green
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
