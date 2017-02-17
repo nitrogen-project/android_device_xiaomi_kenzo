baab59e35b560ec68c5435f46eb7174ba47ebcfe
cb <billchen1977@gmail.com>
2017-02-17 15:34:15 +0800

    Fix makefile typo on init.qcom.qseecomd.sh
    Change-Id: Ic20df4bfe7328681a966200aace8f0fcd2bab6d7

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -82,10 +82,10 @@ LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.qcom.qseecomd.rc
+LOCAL_MODULE		:= init.qcom.qseecomd.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.qcom.qseecomd.rc
+LOCAL_SRC_FILES		:= etc/init.qcom.qseecomd.sh
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
