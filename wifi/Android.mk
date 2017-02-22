9f71f23000bbb3a90240da10f5ac63dbd09f76d1
cb <billchen1977@gmail.com>
2017-02-22 14:25:32 +0800

    Import wifi config from Xiaomi Max
    Change-Id: I50568a293cd72577b8d06679fcf8739d1f0f8e56

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -26,24 +26,4 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
 LOCAL_SRC_FILES    := hostapd.deny
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := WCNSS_cfg.dat
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/firmware/wlan/prima
-LOCAL_SRC_FILES    := WCNSS_cfg.dat
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-
-# Create a link for the WCNSS config file, which ends up as a writable
-# version in /data/misc/wifi
-$(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
-	ln -sf /persist/WCNSS_qcom_wlan_nv.bin \
-	    $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin; \
-	ln -sf /persist/WCNSS_wlan_dictionary.dat \
-	    $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_wlan_dictionary.dat; \
-    ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
-	    $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
-
 endif
