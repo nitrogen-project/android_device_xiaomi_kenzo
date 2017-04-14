ddf26c6c1f15230b892e5bd81e8e5379a183cf4b
xyyx <xyyx@mail.ru>
2017-04-14 13:33:01 +0800

    Revert "init.qcom.post_boot.sh: Use sioplus scheduler"
    This reverts commit 52483388d036e0568622d0552b71a12bc8d897d2.

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -183,4 +183,4 @@ function get-set-forall() {
     write /sys/class/timed_output/vibrator/vtg_level 1800
 
     # Set scheduler
-    write /sys/block/mmcblk0/queue/scheduler sioplus
+    write /sys/block/mmcblk0/queue/scheduler fiops
