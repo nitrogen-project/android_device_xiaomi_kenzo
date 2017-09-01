26058edccc51cfccfa114e2bf848196f1c3d1c78
Bill Chen <billchen1977@gmail.com>
2017-09-01 23:46:15 +0800

    fingerprint: Wrapper force report 2.1 device version
    Change-Id: Ie213495e86a673aa717ec7d5239dfac6a3ba2798
    
    Signed-off-by: xyyx <xyyx@mail.ru>

--- fingerprint/FingerprintWrapper.cpp
+++ fingerprint/FingerprintWrapper.cpp
@@ -228,7 +228,7 @@ static int device_open(const hw_module_t *module, const char *name, hw_device_t 
     }
 
     device->base.common.tag = HARDWARE_DEVICE_TAG;
-    device->base.common.version = device->vendor.device->common.version;
+    device->base.common.version = FINGERPRINT_MODULE_API_VERSION_2_1;
     device->base.common.module = (hw_module_t *) module;
     device->base.common.close = device_close;
 
