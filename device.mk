4476b4039d7e6bf7b2fe17a8c7d2ebb3c3a8eb48
billchen1977@gmail.com <billchen1977@gmail.com>
2017-09-08 18:27:05 +0800

    audio: Use xml audio policy config

--- device.mk
+++ device.mk
@@ -90,7 +90,6 @@ PRODUCT_COPY_FILES += \
 
 # Audio configuration file
 PRODUCT_COPY_FILES += \
-    device/xiaomi/kenzo/audio/audio_policy.conf:system/etc/audio_policy.conf \
     device/xiaomi/kenzo/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     device/xiaomi/kenzo/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     device/xiaomi/kenzo/audio/mixer_paths_mtp.xml:system/etc/mixer_paths_mtp.xml \
@@ -100,6 +99,14 @@ PRODUCT_COPY_FILES += \
     device/xiaomi/kenzo/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     device/xiaomi/kenzo/audio/audio_platform_info_extcodec.xml:system/etc/audio_platform_info_extcodec.xml \
     device/xiaomi/kenzo/audio/aanc_tuning_mixer_wcd9335.txt:system/etc/aanc_tuning_mixer_wcd9335.txt
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
 
 # Audio ACDB stuff
 PRODUCT_COPY_FILES += \
