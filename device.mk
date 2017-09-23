1e205981f6a3a1fa011e0bcd3818c23f4c3c176e
xyyx <xyyx@mail.ru>
2017-09-23 18:05:00 +0800

    Add missed mediaextractor.policy

--- device.mk
+++ device.mk
@@ -321,7 +321,8 @@ PRODUCT_COPY_FILES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
 # IPACM
 PRODUCT_PACKAGES += \
